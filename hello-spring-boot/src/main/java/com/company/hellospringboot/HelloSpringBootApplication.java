5dc121de925886fbdc1bd401e19b3dec3817aaf6
Henry <thehuan@revu.net>
2022-02-15 15:44:38 +0700

    Component Scan

--- hello-spring-boot/src/main/java/com/company/hellospringboot/HelloSpringBootApplication.java
+++ hello-spring-boot/src/main/java/com/company/hellospringboot/HelloSpringBootApplication.java
@@ -1,14 +1,23 @@
 package com.company.hellospringboot;
 
+import com.company.hellospringboot.other_package.Bikini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@ComponentScan("com.company.hellospringboot.other_package")
+@SpringBootApplication(scanBasePackages = {"com.company.hellospringboot.other_package"})
 public class HelloSpringBootApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(HelloSpringBootApplication.class, args);
+
+        Bikini bikini = context.getBean(Bikini.class);
+        System.out.println("Bikini: " + bikini);
+
+        Dress dress = context.getBean(Dress.class); // Failed: No qualifying bean of type 'com.company.hellospringboot.Dress' available
+        System.out.println("Dress: " + dress);
 
         Outfit outfit = context.getBean(Outfit.class);
 
